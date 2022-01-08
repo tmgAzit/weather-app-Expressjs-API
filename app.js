da3e8028accc4352e1152b82296ca4797d564332
tmgAzit <azit.ghising@gmail.com>
2022-01-09 07:37:20 +1100

    weather application complete.

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const https = require('https');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', function (req, res) {
+  const query = req.body.cityName;
+  const appId = 'c415127291870e80419eaccf5a743a7a';
+  const unit = 'metric';
+  const url =
+    'https://api.openweathermap.org/data/2.5/weather?q=' +
+    query +
+    '&appid=' +
+    appId +
+    '&units=' +
+    unit;
+
+  https.get(url, function (response) {
+    response.on('data', function (data) {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const weatherDescription = weatherData.weather[0].description;
+      const weatherIcon = weatherData.weather[0].icon;
+      const imageURL =
+        'http://openweathermap.org/img/wn/' + weatherIcon + '@2x.png';
+
+      res.write(
+        '<h1>The temperature of ' + query + ' is ' + temp + ' celsius.</h1>'
+      );
+
+      res.write(
+        '<p> The weather is currently ' + weatherDescription + '. </p>'
+      );
+
+      res.write('<img src=' + imageURL + '>');
+
+      res.send();
+    });
+  });
+});
+
+app.listen(3000, function () {
+  console.log('Server is running on port 3000.');
+});
